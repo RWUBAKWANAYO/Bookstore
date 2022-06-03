f1b269c6b098bcfeb00e7c1145b56bd16319a0fd
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-03 16:11:28 +0200

    modifier books components

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AddBook from "./AddBook";
 import Book from "./Book";
 
 const Books = () => {
   return (
     <>
       <Book title="Book title" author="Book author" />
+      <AddBook />
     </>
   );
 };
